04d083db33f8908ad420e996d89658187494f731
Jenny Kotanjian <hello@surfcitybotanicals.com>
2023-03-21 21:15:03 -0700

    form added

--- botanicals/views.py
+++ botanicals/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
@@ -9,3 +11,16 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'botanicals/post_detail.html', {'post':post})	
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_data = timezone.now()
+			post.save()
+		return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()		
+	return render(request, 'botanicals/post_edit.html', {'form':form})
